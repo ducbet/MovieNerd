80de559fdc87bd9c08120dd1a0116f840fb57a93
ducbet <ducbet1996@gmail.com>
2020-07-05 11:34:04 +0900

    add_readme

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 module MovieNerd
   class Application < Rails::Application
     config.load_defaults 5.2
-    config.i18n.default_locale = :vi
+    config.i18n.default_locale = :en
     config.i18n.available_locales = [:en, :vi]
     config.action_view.embed_authenticity_token_in_remote_forms = true
     config.assets.initialize_on_precompile = false
